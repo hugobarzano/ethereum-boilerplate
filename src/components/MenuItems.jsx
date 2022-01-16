08dfce317c6227066bdbe1c58e04b10fbe310c69
hugobarzano <hugobarzano@gmail.com>
2022-01-16 18:50:15 +0100

    working in progresss

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,9 +1,9 @@
-import { useLocation } from "react-router";
+//import { useLocation } from "react-router";
 import { Menu } from "antd";
 import { NavLink } from "react-router-dom";
 
 function MenuItems() {
-  const { pathname } = useLocation();
+  //const { pathname } = useLocation();
 
   return (
     <Menu
@@ -16,31 +16,16 @@ function MenuItems() {
         width: "100%",
         justifyContent: "center",
       }}
-      defaultSelectedKeys={[pathname]}
+      defaultSelectedKeys={["nft"]}
     >
-      <Menu.Item key="/quickstart">
-        <NavLink to="/quickstart">🚀 Quick Start</NavLink>
+      <Menu.Item key="nftMarket">
+        <NavLink to="/nftMarket">Explore</NavLink>
       </Menu.Item>
-      <Menu.Item key="/wallet">
-        <NavLink to="/wallet">👛 Wallet</NavLink>
+      <Menu.Item key="nft">
+        <NavLink to="/nft">🖼 Your Porfolio</NavLink>
       </Menu.Item>
-      <Menu.Item key="/1inch">
-        <NavLink to="/1inch">🏦 Dex</NavLink>
-      </Menu.Item>
-      <Menu.Item key="onramp">
-        <NavLink to="/onramp">💵 Fiat</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/erc20balance">
-        <NavLink to="/erc20balance">💰 Balances</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/erc20transfers">
-        <NavLink to="/erc20transfers">💸 Transfers</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/nftBalance">
-        <NavLink to="/nftBalance">🖼 NFTs</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/contract">
-        <NavLink to="/contract">📄 Contract</NavLink>
+      <Menu.Item key="transactions">
+        <NavLink to="/transactions">Transactions</NavLink>
       </Menu.Item>
     </Menu>
   );
